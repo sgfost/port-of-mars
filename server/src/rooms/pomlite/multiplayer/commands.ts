4a1794abd8eacefdd8b195aa4e9a818a61cde3e6
sgfost <sgfost@asu.edu>
2025-11-06 15:08:49 -0700

    feat: extend first round invest time for lite interactive game by 2x

--- server/src/rooms/pomlite/multiplayer/commands.ts
+++ server/src/rooms/pomlite/multiplayer/commands.ts
@@ -113,7 +113,10 @@ export class SetFirstRoundCmd extends CmdWithoutPayload {
     const defaults = this.defaultParams;
     this.state.round = 1;
     this.state.systemHealth = defaults.startingSystemHealth;
-    this.state.timeRemaining = defaults.timeRemaining;
+    this.state.timeRemaining =
+      this.state.type === "prolificInteractive"
+        ? defaults.timeRemaining * 2
+        : defaults.timeRemaining;
     this.state.players.forEach(player => {
       player.resources = defaults.resources;
     });
